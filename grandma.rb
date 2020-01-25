ee71665606f8d00ceed7c8c87919d5255bad8fb4
Claudia Pierson <c172@umbc.edu>
2020-01-25 01:00:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(string)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif string != string.upcase
     return  "HUH?! SPEAK UP, SONNY!" 
-  else  == string.upcase
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
